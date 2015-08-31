92a1c9271b0345c8ccfa8274ce1ab25fa8349a76
itsmelodious <melodyyan16@gmail.com>
2015-08-31 09:46:41 -0700

    Original Design

--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class OriginalDesign extends PApplet {
+
 int starSize;
 int starX;
 int starY;
@@ -5,19 +21,19 @@ int num = 0;
 int x = 100;
 int squareX = 8;
 
-void setup()
+public void setup()
 {
   size(700, 450);
   background(32);
   frameRate(150);
 }
-void draw()
+public void draw()
 {
   star();
   cat();
 }
 
-void star()
+public void star()
 {
   noStroke();
   if (num <= 700)
@@ -36,7 +52,7 @@ void star()
   }
 }
 
-void cat()
+public void cat()
 {
   if ( x < 800)
   {
@@ -85,6 +101,15 @@ void cat()
   if (x == 800)
   {
     x = 100;
-    squareX = 8;
+    squareX = 5;
+  }
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "OriginalDesign" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
   }
 }
